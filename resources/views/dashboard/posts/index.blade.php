0f7548c9f239e081d55a57af5703394c125203d0
Najir Hossain <91908240+hossainnajir@users.noreply.github.com>
2022-01-24 16:04:43 +0600

    First Commit

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -28,9 +28,8 @@
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-xl sm:rounded-lg">
 
-                LIVEWIRE TABLE
-                
-                </table>
+                {{-- Livewire --}}
+                <livewire:post.index>
 
             </div>
         </div>
